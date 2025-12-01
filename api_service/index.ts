7fe77126dbf9e641cab7c3c3b478898b1577eabe
Séthy Laleye <90633415+Sethyshola20@users.noreply.github.com>
2025-12-01 16:23:44 +0100

    go

--- api_service/index.ts
+++ api_service/index.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://solopreneur-tool.vercel.app/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://solopreneur-tool.vercel.app/api';
 export interface RequestOptions extends Omit<RequestInit, 'body'> {
     data?: any;
 }
